84e7f80dd1bdb73b0fc657d5c444f112e33aceaa
AngelicaDavalos <20253rd007@utez.edu.mx>
2026-03-09 13:18:20 -0600

    Creamos el menu principal , y pedimos al usuario que ingrese un aopcion , utilizamos if/else para comparar y verificar la opcion ingresada si es igual a 4 sale del programa si no ingrsa a un swicht para realizar las operaciones o procesos correspondientes , utilizamos do while para que el ciclo se repita hasta que el usuario lo desee

--- Main.java
+++ Main.java
@@ -2,5 +2,26 @@ public class Main (){
     public Static void main (String[]args ){
         PedirDatos pd = new PedirDatos();
         int opcion ; 
+
+        do {
+            System.out.println("-----Menu-----");
+            System.out.println("1-Crear auto");
+            System.out.println("2-Crear camión");
+            System.out.println("3-Mostrar total de vehiculos ");
+            System.out.println("4-Salir");
+
+            opcion = pd.pedirEntero("Selecciona una opcion");
+
+            if (opcion==4 ){
+                System.out.println("BYE");
+            }else {
+                switch (opcion){
+                    
+                }
+            }
+
+
+
+        }while ();
     }
 }
