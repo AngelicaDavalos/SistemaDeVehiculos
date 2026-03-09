81c7bca3260c05b5e4b5b4fde2ec63ed7b22d117
AngelicaDavalos <20253rd007@utez.edu.mx>
2026-03-09 12:52:07 -0600

    Creamos el constructor de la calse auto y sobrescribimos el de la clase vehiculos para mandara traer los atributos de esa clase , tambien creamos los metodos correspondientes de la clase auto

--- Auto.java
+++ Auto.java
@@ -1,5 +1,19 @@
 public class Auto extends Vehiculo{
     protected int numeroPuertas;
     protected String tipoCombustible;
-    
+
+    //constructor
+     public Auto (String marca, String modelo, int anio, double precio, int numeroPuertas, String tipoCombustible){
+        super(marca.modelo,anio,precio);
+        this.numeroPuertas=numeroPuertas;
+        this.tipoCombustible=tipoCombustible;
+     }
+     //metodo 
+
+     public void tocarClaxon (){
+        System.out.println("El auto "+getMarca()+" toca el claxon");
+     }
+     public void mostrarPuertas(){
+        System.out.println("Numero de puertas que tiene el auto  "+this.mostrarPuertas);
+     }
 }
