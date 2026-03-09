8a3070f9f330eedeb15df31de4f627dd027526c1
AngelicaDavalos <20253rd007@utez.edu.mx>
2026-03-09 12:18:35 -0600

    Creamos los atributos de la clase Vehiculo

---
+++ Vehiculo.java
@@ -0,0 +1,9 @@
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+    private int anio ; 
+    private double precio ; 
+    public Static int total Vehiculos =0;
+
+
+}
